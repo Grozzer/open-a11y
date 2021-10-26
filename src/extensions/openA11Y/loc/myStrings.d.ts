dd224f796d2abc320ef0a9ec7a71eb16cca2799e
Chris <christopher.grosberg@waterstons.com>
2021-10-26 21:10:16 +0100

    initial project creation

---
+++ src/extensions/openA11Y/loc/myStrings.d.ts
@@ -0,0 +1,8 @@
+declare interface IOpenA11YApplicationCustomizerStrings {
+  Title: string;
+}
+
+declare module 'OpenA11YApplicationCustomizerStrings' {
+  const strings: IOpenA11YApplicationCustomizerStrings;
+  export = strings;
+}
