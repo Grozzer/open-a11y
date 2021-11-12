bd8d324a0fd85d9eb0185e92d123faebaeee9508
Andrew Buckingham <andrew.buckingham@waterstons.com>
2021-11-12 12:28:59 +0000

    Modal

--- src/extensions/openA11Y/OpenA11yComponent.tsx
+++ src/extensions/openA11Y/OpenA11yComponent.tsx
@@ -1,9 +1,28 @@
-import { Stack } from '@fluentui/react';
+import { DefaultButton, PrimaryButton, Stack } from '@fluentui/react';
+import { useBoolean } from '@fluentui/react-hooks';
+import { ContextualMenu } from '@fluentui/react/lib/ContextualMenu';
+import { Dialog, DialogFooter, DialogType } from '@fluentui/react/lib/Dialog';
 import React from 'react';
 import { ColourComponent } from './modifiers/ColourComponent';
 import { FontComponent } from './modifiers/FontComponent';
 
+const dialogContentProps = {
+  type: DialogType.normal,
+  title: 'Accessibility by open-a11y',
+  closeButtonAriaLabel: 'Close',
+  subText: 'Would you like to enable a more accessible view?',
+};
+
+const dragOptions = {
+  moveMenuItemText: 'Move',
+  closeMenuItemText: 'Close',
+  menu: ContextualMenu,
+  keepInBounds: true,
+};
+
 const OpenA11yComponent: React.FC = () => {
+  const [hideDialog, { toggle: toggleHideDialog }] = useBoolean(true);
+
   const docBody = document.querySelector('body');
   const removeClasses = (elementClasses: string[]) => docBody.classList.remove(...elementClasses);
   const addClass = (elementClass: string) => docBody.classList.add(elementClass);
@@ -11,8 +30,17 @@ const OpenA11yComponent: React.FC = () => {
 
   return (
     <Stack>
-      <FontComponent removeClasses={removeClasses} toggleClass={toggleClass} addClass={addClass} />
-      <ColourComponent removeClasses={removeClasses} toggleClass={toggleClass} addClass={addClass} />
+      <PrimaryButton text="Accessibility" onClick={toggleHideDialog} />
+      <Dialog hidden={hideDialog} onDismiss={toggleHideDialog} dialogContentProps={dialogContentProps} modalProps={{ dragOptions }}>
+        <Stack>
+          <FontComponent removeClasses={removeClasses} toggleClass={toggleClass} addClass={addClass} />
+          <ColourComponent removeClasses={removeClasses} toggleClass={toggleClass} addClass={addClass} />
+        </Stack>
+
+        <DialogFooter>
+          <DefaultButton onClick={toggleHideDialog} text="Done" />
+        </DialogFooter>
+      </Dialog>
     </Stack>
   );
 };
--- src/extensions/openA11Y/modifiers/FontComponent.tsx
+++ src/extensions/openA11Y/modifiers/FontComponent.tsx
@@ -1,12 +1,17 @@
 require('./Style-Fonts.module.scss');
 
 import { Stack } from '@fluentui/react';
-import { DefaultButton } from '@fluentui/react/lib/Button';
+import { DefaultButton, PrimaryButton } from '@fluentui/react/lib/Button';
 import React from 'react';
 import { IOpenA11yBodyClassModifierComponent } from '../IOpenAllyBodyClassModifierComponent';
 
+interface IFontDefinition {
+  name: string;
+  value: string;
+}
+
 // TODO: load dynamically?
-const fonts = [
+const fonts: IFontDefinition[] = [
   { name: 'Arial', value: 'access_font_arial' },
   { name: 'Times New Roman', value: 'access_font_timesnewroman' },
   { name: 'OpenDyslexic', value: 'access_font_opendyslexic' },
@@ -14,16 +19,26 @@ const fonts = [
 ];
 
 const FontComponent: React.FC<IOpenA11yBodyClassModifierComponent> = ({ removeClasses, addClass }) => {
-  const changeFont = (fontValue: string) => {
-    removeClasses(fonts.map((f) => f.value));
-    addClass(fontValue);
+  const [selected, setSelected] = React.useState<IFontDefinition | undefined>(undefined);
+
+  const removeAll = () => removeClasses(fonts.map((f) => f.value));
+
+  const changeFont = (font: IFontDefinition) => {
+    removeAll();
+    addClass(font.value);
+    setSelected(font);
   };
 
   return (
     <Stack horizontal>
-      {fonts.map((f) => (
-        <DefaultButton text={f.name} onClick={() => changeFont(f.value)} />
-      ))}
+      {fonts.map((f) => (selected === f ? <PrimaryButton text={f.name} /> : <DefaultButton text={f.name} onClick={() => changeFont(f)} />))}
+      <DefaultButton
+        text="Normal"
+        onClick={() => {
+          removeAll();
+          setSelected(undefined);
+        }}
+      />
     </Stack>
   );
 };
